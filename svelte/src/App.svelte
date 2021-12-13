5699c8aaa45d912b03487dda7515a52c33a96ddf
ameen424 <ameenpeer23@gmail.com>
2021-12-13 22:21:10 +0530

    svelte

---
+++ svelte/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Feedback from "./components/Feedback.svelte";
+</script>
+
+<main>
+	<Feedback />
+</main>
+
+<style>
+	main {
+		
+		padding: 1em;
+		max-width: 100%;
+		margin: 0 auto;
+	}
+	*{
+		background-color: rgb(6, 6, 126);
+	}
+	
+
+</style>
